0a50725b8956f557f7d541dfba2d50b0b8072749
Yi-Hung Kuo <vincentgouccu@gmail.com>
2021-06-09 00:48:20 +0800

    Replaced backbone with densenet

--- livenessnet.py
+++ livenessnet.py
@@ -8,13 +8,17 @@ from keras.layers.core import Flatten
 from keras.layers.core import Dropout
 from keras.layers.core import Dense
 from keras import backend as K
+from keras.applications.densenet import DenseNet201
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import GlobalAveragePooling2D
 
 class LivenessNet:
     @staticmethod
     def build(width, height, depth, classes):
         # initialize the model along with the input shape to be
         # "channels last" and the channels dimension itself
-        model = Sequential()
+  
         inputShape = (height, width, depth)
         chanDim = -1
 
@@ -23,38 +27,14 @@ class LivenessNet:
         if K.image_data_format() == "channels_first":
             inputShape = (depth, height, width)
             chanDim = 1
+  
+        base_model = DenseNet201(weights='imagenet', include_top=False, input_shape=inputShape)
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(64, activation='relu')(x)
+        predictions = Dense(classes, activation='softmax')(x)
+        model = Model(inputs=base_model.input, outputs=predictions)
 
-        # first CONV => RELU => CONV => RELU => POOL layer set
-        model.add(Conv2D(16, (3, 3), padding="same",
-            input_shape=inputShape))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
-        model.add(Conv2D(16, (3, 3), padding="same"))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.25))
-
-        # second CONV => RELU => CONV => RELU => POOL layer set
-        model.add(Conv2D(32, (3, 3), padding="same"))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
-        model.add(Conv2D(32, (3, 3), padding="same"))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.25))
-
-        # first (and only) set of FC => RELU layers
-        model.add(Flatten())
-        model.add(Dense(64))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization())
-        model.add(Dropout(0.5))
-
-        # softmax classifier
-        model.add(Dense(classes))
-        model.add(Activation("softmax"))
 
         # return the constructed network architecture
         return model
